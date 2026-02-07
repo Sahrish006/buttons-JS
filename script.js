5910913fbbe22428808fd5727f49c25314ac8758
Sahrish006 <sahrish006@gmail.com>
2026-02-07 12:26:26 -0800

    pushing code

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector("#violet").addEventListener("click", function () {
+  document.body.style.backgroundColor = "violet";
+});
+
+document.querySelector("#blue").addEventListener("click", function () {
+  document.body.style.backgroundColor = "CadetBlue";
+});
+
+document.querySelector("#orange").addEventListener("click", function () {
+  document.body.style.backgroundColor = "Orange";
+});
+
+document.querySelector("#pink").addEventListener("click", function () {
+  document.body.style.backgroundColor = "Deeppink";
+});
+
+document.querySelector("#white").addEventListener("click", function () {
+  document.body.style.backgroundColor = "White";
+});
